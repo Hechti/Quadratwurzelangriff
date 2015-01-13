a51c0905d47a5a4954705e3e437091b7a58d9eff
Hechti <dennyhecht@gmail.com>
2015-01-13 21:53:08 +0100

    changed pointer from params to reference

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,35 +1,50 @@
 #include "InfInt.h"
-#include <map>
+#include <list>
 using namespace std;
 
-void powModulo(InfInt *basis, InfInt *exponent, InfInt *modulus, InfInt *result)
+void powModulo(const InfInt &basis, const InfInt &exponent, const InfInt &modulus, InfInt &result)
 {
     InfInt check1 = 0;
     InfInt check2 = 1;
 
-    if (*basis == check1)
+    if (basis == check1)
     {
-        *result = check1;
+        result = check1;
         return;
     }
 
-    if (*exponent == check1)
+    if (&exponent == check1)
     {
-        *result = check2;
+        result = check2;
         return;
     }
 
-    if (*exponent == check2)
+    if (exponent == check2)
     {
-        *result = *basis;
+        result = basis;
         return;
     }
 
     map<InfInt, InfInt> values;
+    InfInt currentExp = 1;
+    InfInt globalExp = 1;
+    result = basis;
 
-    for (InfInt exp = 1; exp != *exponent; exp *= 2)
+    do
     {
+        globalExp *= 2;
         
-    }
+        if ((globalExp - exponent) > 0)
+        {
+            result *= result;
+            currentExp *= 2;
+
+//             values.push_front(result;
+        }
+        else
+        {
+            
+        }
+    } while (globalExp != exponent);
 }
 
--- Utilities.h
+++ Utilities.h
@@ -1,6 +1,6 @@
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 
-void powModulo(InfInt *basis, InfInt *exponent, InfInt *modulus, InfInt *result);
+void powModulo(InfInt &basis, InfInt &exponent, InfInt &modulus, InfInt &result);
 
 #endif
