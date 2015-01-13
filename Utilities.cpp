d450e294e9ba87671ccf6c4b771e38882e5fbec0
kfe <kfe@kfe.de>
2015-01-13 19:29:45 +0100

    add powInfInt

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,6 +1,7 @@
-#include "InfInt.h"
 #include <map>
 using namespace std;
+
+#include "Utilities.h"
 
 void powModulo(InfInt *basis, InfInt *exponent, InfInt *modulus, InfInt *result)
 {
@@ -33,3 +34,25 @@ void powModulo(InfInt *basis, InfInt *exponent, InfInt *modulus, InfInt *result)
     }
 }
 
+void powInfInt(const InfInt& base, const InfInt& exp, InfInt& result)
+{
+	unsigned long long expll = exp.toUnsignedLongLong();
+
+	int i;
+	for (i = 62; i>=1; --i)
+	{
+		if (((expll>>i)&1) == 1)
+		{
+			break;
+		}
+	}
+	result = base;
+	for (--i; i >=0; --i)
+	{
+		result = result * result;
+		if ((expll>>i)&1)
+		{
+			result = result * base;
+		}
+	}
+}
