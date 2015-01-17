a0ec9352536fffede234720592c98bfa59aeb574
Hechti <dennyhecht@gmail.com>
2015-01-17 22:26:36 +0100

    improved lock.h

--- Lock.h
+++ Lock.h
@@ -7,18 +7,24 @@
 struct Lock 
 {
     int *mutex;
+    int *isResultFound;
 
     Lock()
     {
         int state = 0;
-
         CHECK(cudaMalloc((void**) &mutex, sizeof(int)));
         CHECK(cudaMemcpy(mutex, &state, sizeof(int), cudaMemcpyHostToDevice));
+
+        int result = 0;
+        CHECK(cudaMalloc((void**) &isResultFound, sizeof(int)));
+        CHECK(cudaMemcpy(isResultFound, &result, sizeof(int), cudaMemcpyHostToDevice));
+
     }
     
     ~Lock()
     {
         cudaFree(mutex);
+        cudaFree(isResultFound);
     }
     
     __device__ void lock()
@@ -28,6 +34,17 @@ struct Lock
     __device__ void unlock()
     { 
         atomicExch(mutex, 0);
+    }
+
+    __device__ void foundResult(int &result)
+    {
+        result = *isResultFound;
+    }
+
+    __device__ void setFoundResult(int &result)
+    {
+        *isResultFound = 1;
+        result = *isResultFound;
     }
 };
 
